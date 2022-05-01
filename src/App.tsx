726da12f77d3dc49cb3a5bfbddc391e34fde6cfc
William Olson <willko747@gmail.com>
2022-05-01 17:31:38 -0500

    chore: cleanup

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,12 @@
-import React, { ReactElement, useState } from 'react'
+import { ReactElement } from 'react'
 import logo from './logo.svg'
 
 function App(): ReactElement {
-  const [count, setCount] = useState(0)
-
   return (
     <div className="border border-gray-50 rounded-xl p-20 shadow-xl">
       <header>
         <div className="flex justify-center">
-          <img src={logo} className="h-32 w-32 animate-spin-slow" alt="logo" />
+          <img src={logo} className="h-32 w-32 hover:animate-spin" alt="logo" />
         </div>
         <p className="text-2xl font-mono pb-3">Olson Studio</p>
         <p></p>
