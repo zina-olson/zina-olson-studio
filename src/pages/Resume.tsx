2fd7e3e9ff018f4d9a4cb5de8fa16e169882525a
William Olson <vazina.olson@gmail.com>
2022-05-24 11:03:19 -0500

    chore: adding content

--- src/pages/Resume.tsx
+++ src/pages/Resume.tsx
@@ -4,17 +4,105 @@ export class Resume extends React.Component {
   public render() {
     return (
       <div>
-        <p className="">
-          Resume It is a long established fact that a reader will be distracted
-          by the readable content of a page when looking at its layout. The
-          point of using Lorem Ipsum is that it has a more-or-less normal
-          distribution of letters, as opposed to using 'Content here, content
-          here', making it look like readable English. Many desktop publishing
-          packages and web page editors now use Lorem Ipsum as their default
-          model text, and a search for 'lorem ipsum' will uncover many web sites
-          still in their infancy. Various versions have evolved over the years,
-          sometimes by accident, sometimes on purpose (injected humour and the
-          like).
+        <header className="text-center">
+          <p className="text-2xl font-mono font-semibold pb-12">
+            Software QA Automation Engineer
+          </p>
+        </header>
+        <p className="text-lg font-semibold pb-4">Professional Summary</p>
+        <p className="text-md font-semibold">
+          Experienced Software Quality Assurance Engineer with 5+ years of
+          experience in Manual and Automation testing for web, client/server,
+          cloud based and mobile applications. Extensive knowledge of functional
+          and non-functional testing, SDLC, STLC, software methodologies such as
+          Agile, Scrum, and Waterfall. Solid knowledge of testing tools such as
+          Selenium WebDriver, Postman API, Swagger, Lighthouse, GT Metrix,
+          SpeedLab, Git, Jira, PSQL, Python and JavaScript.
+          <p className="pb-6"></p>
+          <p className="text-lg font-semibold pb-4">Experience </p>
+          <p className="text-md font-semibold">
+            Implementation Engineer at Iodine Software, Austin, TX May 2021 -
+            May 2022
+            <p className="pb-4"></p>
+            <p className="text-md font-semibold">
+              - Functional and non-functional testing, bug reporting through
+              Jira.
+            </p>
+            <p className="">
+              - Deployed a newly integrated solution as part of the beta testing
+              team.
+            </p>
+            <p className="">
+              - Implementation track responsibilities: Site-to-site VPN and
+              interfaces connectivity testing, data validation and analysis,
+              application deployments, setting up sFTP sites, setting up Azure
+              SAML and LDAP authentications, contributing to documentation
+              improvement.
+              <p className="">
+                - Project Management responsibilities: data availability
+                discoveries, analyzing and proposing project scope and timeline;
+                presenting the proposed deliverables to the stakeholders.
+              </p>
+            </p>
+            <p className="">
+              - Troubleshooting support for client success and support teams.
+            </p>
+            <p className="">
+              - Leading cross-functional teams to achieve project goals and
+              objectives within the agreed upon scope, timelines and budget.
+              Tracked projects in Smartsheet.
+            </p>
+            <p className="">
+              - Completed three projects starting at $1.3M each. Led 7 smaller
+              scale ad-hoc projects.
+            </p>
+            <p className="pb-4"></p>
+            <p className="text-md font-bold">
+              Technical Account Manager at Yardi Systems, Austin, TX Aug 2017 -
+              May 2021
+              <p className="pb-4"></p>
+              <p className="text-md font-semibold">
+                <p className="">
+                  - Evaluated client reports and drove communication with
+                  Development teams to confirm bug fixes and potential
+                  functional enhancements in upcoming releases.
+                </p>
+                <p className="">
+                  - Wrote SQL scripts, product customizations, modified reports,
+                  tracked projects in CRM.
+                </p>
+                <p className="">
+                  - Collaborated with a team of 5 on manual testing product
+                  compatibility with third-party SSO systems such as Okta,
+                  connectivity, and functionality testing of new products.
+                </p>
+                <p className="">
+                  - Produced over 47 documentations and traceability matrices
+                  for positive, negative and AdHoc functional bugs and delivered
+                  100+ HotFix requests or workarounds to clients.
+                </p>
+              </p>
+              <p className="pb-6"></p>
+              <p className="text-lg font-semibold pb-4">
+                Skills and Interests:
+              </p>
+              Selenium WebDriver, Postman API, Swagger, Lighthouse, GT Metrix,
+              SpeedLab, Python, JavaScript, XPath, XML, HTML5, CSS3, JSON,
+              PyCharm, WebStorm, VScode, Regex, Postgresql, Mozilla Observatory,
+              Agile, Scrum, Waterfall, GitHub, Git, Jira, Confluence, CRM,
+              Windows, iOS, Android, Netlify, Tailwindcss.
+              <p className="pb-6"></p>
+              <p className="text-lg font-semibold pb-4">Education:</p>
+              Bachelor of Science in Computer Science, Texas State University.
+              Aug 2014 - Dec 2016
+              <p className=""></p>
+              Master of Engineering Technical Management, Texas A&M University.
+              Aug 2019 - May 2021
+              <p className=""></p>
+              QA Automation Engineer Course at Silicon Valley, CA, QAVS.US April
+              2022-Present
+            </p>
+          </p>
         </p>
       </div>
     );
